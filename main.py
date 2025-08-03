9a2c8f5d2b647f96fd35079b86d305f4b6edc31b
shannoj <69207315+shannoj@users.noreply.github.com>
2025-08-03 08:48:03 -0700

    further cleaned the QB stats to remove receptions

--- main.py
+++ main.py
@@ -14,5 +14,12 @@ mask = PlayerStats[position] == Quarterbacks
 
 PlayerStats_QB = PlayerStats[mask]
 
-print("\nFiltered DataFrame (keeping 'QB'):")
-print(PlayerStats_QB)
+drop_empty_cols = [col for col in PlayerStats_QB.columns if (PlayerStats_QB[col] == 0).all()]
+
+PlayerStats_QB_cleaned = PlayerStats_QB.drop(columns=drop_empty_cols)
+
+Qb_Stats = PlayerStats_QB_cleaned.drop(columns=['receptions', 'targets','receiving_yards','receiving_air_yards','receiving_yards_after_catch','receiving_first_downs','receiving_epa','receiving_2pt_conversions', 'target_share','air_yards_share'])
+
+print(Qb_Stats.columns)
+
+
