e376cdad6299bc4879ca4afc1c4c2810e2e4df89
shannoj <69207315+shannoj@users.noreply.github.com>
2025-08-03 14:57:26 -0700

    removed features based off cat boost graph and tried to increase R squared score using Cat Boost

---
+++ Cleaning.py
@@ -0,0 +1,23 @@
+def QB_cleaning(file, is_2022 = False):
+
+    position = 'position'
+    Quarterbacks = 'QB'
+
+    mask = file[position] == Quarterbacks
+
+    PlayerStats_QB = file[mask]
+
+    drop_empty_cols = [col for col in PlayerStats_QB.columns if (PlayerStats_QB[col] == 0).all()]
+
+    PlayerStats_QB_cleaned = PlayerStats_QB.drop(columns=drop_empty_cols)
+
+    PlayerStats_QB_cleaned_2 = PlayerStats_QB_cleaned.dropna(axis=1, how='all')
+
+    if is_2022:
+        Qb_Stats = PlayerStats_QB_cleaned_2.drop(columns=['passing_2pt_conversions','receiving_tds','sack_fumbles_lost','sacks','sack_fumbles','rushing_first_downs','rushing_fumbles_lost','rushing_fumbles','carries','rushing_epa','passing_yards_after_catch','passing_air_yards','week','rushing_2pt_conversions','sack_yards','season','opponent_team','season_type','recent_team','position_group','player_display_name','player_name','position','fantasy_points','racr','wopr','headshot_url','receptions', 'targets','receiving_yards','receiving_air_yards','receiving_yards_after_catch','receiving_first_downs','receiving_epa', 'target_share','air_yards_share'])
+    else:
+        Qb_Stats = PlayerStats_QB_cleaned_2.drop(columns=['passing_2pt_conversions','sack_fumbles_lost','sacks','sack_fumbles','rushing_first_downs','rushing_fumbles_lost','rushing_fumbles','carries','rushing_epa','passing_yards_after_catch','passing_air_yards','week','rushing_2pt_conversions','sack_yards','season','opponent_team','season_type','recent_team','position_group','player_display_name','player_name','position','fantasy_points','racr','wopr','headshot_url','receptions', 'targets','receiving_yards','receiving_air_yards','receiving_yards_after_catch','receiving_first_downs','receiving_epa','receiving_2pt_conversions', 'target_share','air_yards_share'])
+    return Qb_Stats
+
+def add_year_suffix(df, year):
+    return df.rename(columns={col: f"{col}_{year}" for col in df.columns if col != 'player_id'})
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import pandas as pd
-from QB_cleaning import QB_cleaning
+from Cleaning import QB_cleaning
+from Cleaning import add_year_suffix
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
+from sklearn.impute import SimpleImputer
+import xgboost as xgb
+from xgboost import XGBRegressor
+from sklearn.model_selection import cross_val_score
+from catboost import CatBoostRegressor
+import matplotlib.pyplot as plt
+
+
+
+imputer = SimpleImputer(strategy='mean')
 
 PlayerStats2021 = pd.read_csv("../FF_DATA_SCIENCE_PROJECT/player_stats_2021.csv")
 PlayerStats2022 = pd.read_csv("../FF_DATA_SCIENCE_PROJECT/player_stats_2022.csv")
@@ -11,6 +27,44 @@ QbStats2022 = QB_cleaning(PlayerStats2022, is_2022=True)
 QbStats2023 = QB_cleaning(PlayerStats2023)
 QbStats2024 = QB_cleaning(PlayerStats2024, is_2022=True)
 
-QbStatsTotal = pd.merge(QbStats2021, QbStats2022, QbStats2023, QbStats2024, on='player_id', how='inner')
+QbStats2021 = add_year_suffix(QbStats2021, 2021)
+QbStats2022 = add_year_suffix(QbStats2022, 2022)
+QbStats2023 = add_year_suffix(QbStats2023, 2023)
+QbStats2024 = add_year_suffix(QbStats2024, 2024)
 
-print(QbStatsTotal.head())
+
+QbStatsTotal = pd.merge(QbStats2021, QbStats2022, on='player_id', how='inner')
+QbStatsTotal = pd.merge(QbStatsTotal, QbStats2023, on='player_id', how='inner')
+QbStatsTotal = pd.merge(QbStatsTotal, QbStats2024, on='player_id', how='inner')
+
+QbStatsML = QbStatsTotal.dropna(subset=['fantasy_points_ppr_2024'])
+
+QbStatsML2 = QbStatsML[~(QbStatsML == 0).all(axis=1)]
+
+y = QbStatsML2['fantasy_points_ppr_2024']
+
+X = QbStatsML2[[col for col in QbStatsML.columns if 
+               any(year in col for year in ['2021', '2022', '2023']) and
+               'fantasy_points_ppr' not in col and
+               col != 'player_id']]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1)
+
+model = CatBoostRegressor(verbose=0, random_seed=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+
+print(f"R² Score: {r2_score(y_test, y_pred):.2f}")
+print(f"RMSE: {np.sqrt(mean_squared_error(y_test, y_pred)):.2f}")
+
+feature_importances = model.get_feature_importance()
+feature_names = X.columns
+
+plt.figure(figsize=(12, 6))
+plt.barh(feature_names, feature_importances)
+plt.xlabel("Importance")
+plt.title("Feature Importance from CatBoost")
+plt.tight_layout()
+plt.show()
