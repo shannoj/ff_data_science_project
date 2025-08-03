fdcada929521087f4a250a32050407c146a03618
shannoj <69207315+shannoj@users.noreply.github.com>
2025-08-03 07:57:14 -0700

    updated the stats I was using

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import pandas as pd
 
-BasicStats = pd.read_csv("../FF_DATA_SCIENCE_PROJECT/stats/Basic_Stats.csv")
-GameLogsRunning = pd.read_csv("../FF_DATA_SCIENCE_PROJECT/stats/Game_Logs_Runningback.csv")
+PlayerStats = pd.read_csv("../FF_DATA_SCIENCE_PROJECT/player_stats_2021.csv")
 
-GameLogsRunning.columns = [c.replace(' ', '_') for c in GameLogsRunning.columns]
-print(GameLogsRunning.columns)
-
-y = GameLogsRunning.Rushing_Yards
-print(y.head())
+print(PlayerStats.head())
+print(PlayerStats.columns)
 
