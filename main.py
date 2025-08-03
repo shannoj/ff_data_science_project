f8bc8f04307eb6931572c91d619cab83ffb30c37
shannoj <69207315+shannoj@users.noreply.github.com>
2025-08-03 08:13:36 -0700

    filtered for QB stats

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ PlayerStats = pd.read_csv("../FF_DATA_SCIENCE_PROJECT/player_stats_2021.csv")
 print(PlayerStats.head())
 print(PlayerStats.columns)
 
+print(PlayerStats.position.head())
+
+position = 'position'
+Quarterbacks = 'QB'
+
+mask = PlayerStats[position] == Quarterbacks
+
+PlayerStats_QB = PlayerStats[mask]
+
+print("\nFiltered DataFrame (keeping 'QB'):")
+print(PlayerStats_QB)
