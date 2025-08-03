1a6aaef508857ceb84399ba769f67a47b753b31a
shannoj <69207315+shannoj@users.noreply.github.com>
2025-08-03 09:15:03 -0700

    made data cleaning function to get QB data out of the player stats

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
 import pandas as pd
+from QB_cleaning import QB_cleaning
 
 PlayerStats = pd.read_csv("../FF_DATA_SCIENCE_PROJECT/player_stats_2021.csv")
 
-print(PlayerStats.head())
-print(PlayerStats.columns)
+QbStats2021 = QB_cleaning(PlayerStats)
 
-print(PlayerStats.position.head())
-
-position = 'position'
-Quarterbacks = 'QB'
-
-mask = PlayerStats[position] == Quarterbacks
-
-PlayerStats_QB = PlayerStats[mask]
-
-drop_empty_cols = [col for col in PlayerStats_QB.columns if (PlayerStats_QB[col] == 0).all()]
-
-PlayerStats_QB_cleaned = PlayerStats_QB.drop(columns=drop_empty_cols)
-
-Qb_Stats = PlayerStats_QB_cleaned.drop(columns=['receptions', 'targets','receiving_yards','receiving_air_yards','receiving_yards_after_catch','receiving_first_downs','receiving_epa','receiving_2pt_conversions', 'target_share','air_yards_share'])
-
-print(Qb_Stats.columns)
-
-
+print(QbStats2021.head())
+print(QbStats2021.columns)
