8dd254c48fbcf1b71eba246c532307f1f3000654
shannoj <69207315+shannoj@users.noreply.github.com>
2025-08-05 21:45:47 -0700

    testing on just the 2021 season

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ import matplotlib.pyplot as plt
 from features import PassingPoints
 from features import RushingPoints
 from features import TDPoints
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(strategy='mean')
 
 PlayerStats2021 = pd.read_csv("../FF_DATA_SCIENCE_PROJECT/player_stats_2021.csv")
 PlayerStats2022 = pd.read_csv("../FF_DATA_SCIENCE_PROJECT/player_stats_2022.csv")
@@ -21,38 +25,43 @@ PlayerStats2022 = PlayerStats2022.drop_duplicates()
 PlayerStats2023 = PlayerStats2023.drop_duplicates()
 PlayerStats2024 = PlayerStats2024.drop_duplicates()
 
-QbStats2021 = TDPoints(RushingPoints(PassingPoints(QB_cleaning(PlayerStats2021))))
+print(PlayerStats2021.passing_yards)
+
+QbStats2021 = QB_cleaning(PlayerStats2021)
 QbStats2022 = TDPoints(RushingPoints(PassingPoints(QB_cleaning(PlayerStats2022, is_2022=True))))
 QbStats2023 = TDPoints(RushingPoints(PassingPoints(QB_cleaning(PlayerStats2023))))
 QbStats2024 = TDPoints(RushingPoints(PassingPoints(QB_cleaning(PlayerStats2024, is_2022=True))))
+
+print(QbStats2021.passing_yards)
 
 QbStats2021 = add_year_suffix(QbStats2021, 2021)
 QbStats2022 = add_year_suffix(QbStats2022, 2022)
 QbStats2023 = add_year_suffix(QbStats2023, 2023)
 QbStats2024 = add_year_suffix(QbStats2024, 2024)
 
+print(QbStats2021.passing_yards_2021)
+
 QbStatsTotal = pd.merge(QbStats2021, QbStats2022, on='player_id', how='inner')
 QbStatsTotal = pd.merge(QbStatsTotal, QbStats2023, on='player_id', how='inner')
 QbStatsTotal = pd.merge(QbStatsTotal, QbStats2024, on='player_id', how='inner')
 
-QbStatsML = QbStatsTotal.dropna(subset=['fantasy_points_ppr_2024'])
+QbStatsML = QbStats2021.dropna(subset=['fantasy_points_ppr_2021'])
 
 QbStatsML2 = QbStatsML[~(QbStatsML == 0).all(axis=1)]
 
-y = QbStatsML2['fantasy_points_ppr_2024']
+y = QbStatsML2['fantasy_points_ppr_2021']
 
 X = QbStatsML2[[col for col in QbStatsML.columns if 
                any(year in col for year in ['2021', '2022', '2023']) and
-               'fantasy_points_ppr' not in col and
                col != 'player_id']]
 
-print(X.head(10))
+X = X.drop('fantasy_points_ppr_2021', axis=1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1)
+X_imputed = imputer.fit_transform(X)
 
-print(y_train)
+X_train, X_test, y_train, y_test = train_test_split(X_imputed, y, test_size=0.20, random_state=1)
 
-model = CatBoostRegressor(verbose=0, random_seed=42)
+model = RandomForestRegressor(n_estimators=20, random_state=42)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
