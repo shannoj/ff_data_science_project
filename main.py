4af217ea0d8d6ea6d5d46cc1d98af4f26e5527bb
James Shannon <69207315+shannoj@users.noreply.github.com>
2025-12-18 12:56:40 -0800

    added defensive statistics

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 from Cleaning import position_cleaning, handle_categoricals, add_year_suffix
 from model import predict_category
 
-PlayerStats2024 = pd.read_csv("../FF_DATA_SCIENCE_PROJECT/player_stats_2024_with_home_away.csv")
+PlayerStats2024 = pd.read_csv("../FF_DATA_SCIENCE_PROJECT/enhanced_stats.csv")
 
 QbStats2024 = (position_cleaning(PlayerStats2024, 'QB'))
 
@@ -15,6 +15,9 @@ r2_passing_yard, rmse_passing_yards, passing_yards_model = predict_category('pas
 r2_pass_td, rmse_pass_td, passing_td_model = predict_category('passing_tds_2024', QbStats2024_encoded)
 
 print(r2_pass_td)
+
+print(r2_passing_yard)
+
 
 PlayerStats2024 = pd.read_csv("../FF_DATA_SCIENCE_PROJECT/player_stats_2024.csv")
 
@@ -29,3 +32,21 @@ r2_passing_yard, rmse_passing_yards, passing_yards_model = predict_category('pas
 r2_pass_td, rmse_pass_td, passing_td_model = predict_category('passing_tds_2024', QbStats2024_encoded)
 
 print(r2_pass_td)
+
+print(r2_passing_yard)
+
+PlayerStats2024 = pd.read_csv("../FF_DATA_SCIENCE_PROJECT/player_stats_2024_with_home_away.csv")
+
+QbStats2024 = (position_cleaning(PlayerStats2024, 'QB'))
+
+QbStats2024 = add_year_suffix(QbStats2024, 2024)
+
+QbStats2024_encoded = handle_categoricals(QbStats2024)
+
+r2_passing_yard, rmse_passing_yards, passing_yards_model = predict_category('passing_yards_2024', QbStats2024_encoded)
+
+r2_pass_td, rmse_pass_td, passing_td_model = predict_category('passing_tds_2024', QbStats2024_encoded)
+
+print(r2_pass_td)
+
+print(r2_passing_yard)
